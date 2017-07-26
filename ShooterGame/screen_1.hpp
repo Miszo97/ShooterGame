29afa7596f208e2256ded90e748712d01c2d5493
Miszo97 <kol478@gmail.com>
2017-07-26 15:45:17 +0200

    Correct Switching

--- ShooterGame/screen_1.hpp
+++ ShooterGame/screen_1.hpp
@@ -6,82 +6,232 @@
 class screen_1 : public cScreen
 {
 private:
-    float movement_step;
-    float posx;
-    float posy;
-    sf::RectangleShape Rectangle;
-public:
+    bool Running = true;
+    sf::Texture  array_of_body_textuers [1];
+    sf::Texture shoot_rifle[3];
+    sf::Texture bullet_texture;
+    sf::Texture enemy_texture;
+    sf::Music music;
+    sf::Clock clock;
+    sf::Time enemy_spawn = sf::seconds(1);
+    Map map;
+    sf::Font font;
+    sf::Text elapsed_time;
+    sf::Text hp_text;
+    
+    public:
+    
     screen_1(void);
     virtual int Run(sf::RenderWindow &App);
 };
 
 screen_1::screen_1(void)
 {
-    movement_step = 5;
-    posx = 320;
-    posy = 240;
-    //Setting sprite
-    Rectangle.setFillColor(sf::Color(255, 255, 255, 150));
-    Rectangle.setSize({ 10.f, 10.f });
+    
+    array_of_body_textuers[0].loadFromFile(resourcePath() + "/images/survivor.png");
+    
+    sf::Texture shoot_rifle[3];
+    
+    
+    shoot_rifle[0].loadFromFile(resourcePath() + "/shoot/survivor-shoot_rifle_0.png");
+    shoot_rifle[1].loadFromFile(resourcePath() + "/shoot/survivor-shoot_rifle_1.png");
+    shoot_rifle[2].loadFromFile(resourcePath() + "/shoot/survivor-shoot_rifle_2.png");
+    
+    
+    bullet_texture.loadFromFile(resourcePath() + "/images/bullet.png");
+    enemy_texture.loadFromFile(resourcePath() + "/images/enemy_texture.png");
+    
+   
+    
+    // Open it from an audio file
+    if (!music.openFromFile(resourcePath() + "gun_sound.wav"))
+    {
+        // error...
+    }
+    
+
+    map.player.loadSprites(array_of_body_textuers, shoot_rifle);
+
+    map.createEnemy(enemy_texture);
+    //map.createEnemy(enemy_texture);
+    
+    
+    
+    
+    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
+        return EXIT_FAILURE;
+    }
+    
+   
+    elapsed_time.setFont(font);
+    elapsed_time.setPosition(700, 30);
+    hp_text.setFont(font);
+    hp_text.setPosition(600, 30);
 }
 
 int screen_1::Run(sf::RenderWindow &App)
 {
-    sf::Event Event;
-    bool Running = true;
+    
+    
+    
+
     
     while (Running)
     {
+        sf::Event event;
         //Verifying events
-        while (App.pollEvent(Event))
+        while (App.pollEvent(event))
         {
             // Window closed
-            if (Event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed)
             {
                 return (-1);
             }
-            //Key pressed
-            if (Event.type == sf::Event::KeyPressed)
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                App.close();
+            }
+            
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                App.close();
+            }
+            
+            
+            if (event.type == sf::Event::MouseButtonPressed)
             {
-                switch (Event.key.code)
+                sf::Vector2i localPosition = sf::Mouse::getPosition(App);
+                map.createBullet(bullet_texture, map.player.rotation, localPosition);
+                map.player.Shoot();
+                music.play();
+            }
+            
+            
+            
+            
+            
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+            {
+                map.createEnemy(enemy_texture);
+            }
+            
+            
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                
+                map.player.moveLeft();
+                
+                map.player.isGoingLeft = true;
+            } else {
+                map.player.isGoingLeft = false;
+            }
+            
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            {
+                
+                map.player.moveRight();
+                
+                map.player.isGoingRight = true;
+                
+            } else {
+                map.player.isGoingRight = false;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            {
+                
+                map.player.moveUp();
+                
+                map.player.isGoingForward = true;
+                
+                
+                
+            } else{
+                map.player.isGoingForward = false;
+            }
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            {
+                
+                map.player.moveDown();
+                map.player.isGoingBackward = true;
+                
+                
+            } else {
+                map.player.isGoingBackward = false;
+            }
+        }
+        
+        sf::Time elapsed = clock.getElapsedTime();
+        hp_text.setString(std::to_string(floor(elapsed.asSeconds())));
+        
+        if (elapsed.asSeconds() > enemy_spawn.asSeconds()) {
+            map.createEnemy(enemy_texture);
+            enemy_spawn += sf::seconds(1);
+        }
+        
+        
+        
+        
+        
+        App.clear(sf::Color(123,164,98));
+        
+        sf::Vector2i localPosition = sf::Mouse::getPosition(App);
+        map.player.updateRotation(localPosition);
+        map.player.UpdatePossision();
+        
+        
+        if (!(map.Enemys.empty())) {
+            for (auto a = map.Enemys.begin(); a != map.Enemys.end(); ++a){
+                if (a->abilityToAtack() == true) map.player.reduceHP(10);
+                a->UpdatePossision(map.player.possision);
+                a->updateRotation(map.player.possision);
+            }
+        }
+        
+        
+        if (!(map.Bullets.empty())) {
+            for (auto a = map.Bullets.begin(); a != map.Bullets.end();) {
+                
+                if(a->possision.x > 800 || a->possision.x < 0) map.Bullets.erase(a);
+                else
                 {
-                    case sf::Keyboard::Escape:
-                        return (0);
-                        break;
-                    case sf::Keyboard::Up:
-                        posy -= movement_step;
-                        break;
-                    case sf::Keyboard::Down:
-                        posy += movement_step;
-                        break;
-                    case sf::Keyboard::Left:
-                        posx -= movement_step;
-                        break;
-                    case sf::Keyboard::Right:
-                        posx += movement_step;
-                        break;
-                    default:
-                        break;
+                    a->UpdatePossision();
+                    
+                    ++a;
                 }
             }
         }
         
-        //Updating
-        if (posx>630)
-            posx = 630;
-        if (posx<0)
-            posx = 0;
-        if (posy>470)
-            posy = 470;
-        if (posy<0)
-            posy = 0;
-        Rectangle.setPosition({ posx, posy });
+        map.Bullet_Enemy_Coll();
         
-        //Clearing screen
-        App.clear(sf::Color(0, 0, 0, 0));
-        //Drawing
-        App.draw(Rectangle);
+        
+        
+        
+        if (!(map.Bullets.empty())) {
+            for (auto a = map.Bullets.begin(); a != map.Bullets.end(); ++a) App.draw(*a);
+        }
+        
+        if (!(map.Enemys.empty())) {
+            for (auto a = map.Enemys.begin(); a != map.Enemys.end(); ++a) App.draw(*a);
+        }
+        
+        
+        hp_text.setString(std::to_string(map.player.hp));
+        
+        App.draw(map.player);
+        
+        App.draw(elapsed_time);
+        App.draw(hp_text);
+        
+        
+        
+        
+        // Update the window
         App.display();
+
     }
     
     //Never reaching this point normally, but just in case, exit the application
